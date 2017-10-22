93036d45f6c121aef4b508ad5893204b65affa5d
noobtube2 <noobtube2@live.co.uk>
2017-10-23 00:05:32 +0100

    Add files via upload

---
+++ discordBot/discordBot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        private CommandHandler _handler;
+
+        public async Task StartAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            new CommandHandler();
+
+            await _client.LoginAsync(TokenType.Bot, "MzcxNjg2Mjk0MTkyOTE0NDM0.DM5e0Q.0GTIaDCHGlmDGHtj_MpCk0FpzJI");
+
+            await _client.StartAsync();
+
+            _handler = new CommandHandler();
+
+            await _handler.InitializeAsync(_client);
+
+            await Task.Delay(-1);
+        }
+
+    }
+
+}
---
+++ discordBot/discordBot/modules/Test2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Commands;
+using System.Threading.Tasks;
+using Discord.Net;
+
+namespace discordBot.Modules
+{
+    public class Test2 : ModuleBase<SocketCommandContext>
+    {
+        [Command("Test")]
+
+        public async Task Asdasd()
+        {
+            await Context.Channel.SendMessageAsync("test success!");
+        }
+
+
+        [Command("English")]
+        public async Task Asdf()
+        {
+            await Context.User.
+        }
+    }
+}
